c52644ccaac026d04b5b32fd836230ac7572daf9
benclough724 <ben_clough123@outlook.com>
2024-12-09 20:49:40 +0000

    Implemented text to speech
    Implemented text to speech using the openai text to speech api.

---
+++ openai/env/TTS.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from pathlib import Path
+from openai import OpenAI
+client = OpenAI()
+
+speech_file_path = Path(__file__).parent / "speech.mp3"
+response = client.audio.speech.create(
+  model="tts-1",
+  voice="alloy",
+  input="I am sat doing some coding. My pc is attached to my monitor which is attached to the wall."
+)
+
+response.stream_to_file(speech_file_path)
