bf298abebe5ba4e45b0d83523eb276ac722d49e2
benclough724 <ben_clough123@outlook.com>
2024-12-12 19:27:47 +0000

    Update Page.js
    Updated style of the page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -11,10 +11,11 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-blue-500 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Text to speech app</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className="">
             <input
               type="text"
               value={inputValue}
