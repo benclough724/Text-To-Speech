e8162ba193200b0b995ba8a0edf8eb82b27de7ef
benclough724 <ben_clough123@outlook.com>
2024-12-13 20:11:20 +0000

    u
    u

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -15,14 +15,21 @@ export default function Home() {
       <h1>Text to speech app</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          <form onSubmit={handleSubmit} className="">
+          <form
+            onSubmit={handleSubmit}
+            className="w-full flex flex-col md:flex-row justify-between items-center md:space-x-4 space-y-4 md:space-y-0">
             <input
               type="text"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
               placeholder="Enter your text"
+              className="flex-1 h-40 border border-gray-300 rounded-lg p-2"
             />
-            <button type="submit">Submit</button>
+            <button
+              type="submit"
+              className="p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+              Submit
+            </button>
           </form>
         </div>
       </main>
