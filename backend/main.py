58eb967e04c764eb6d6bba72957212ba0b9d050a
benclough724 <ben_clough123@outlook.com>
2024-12-15 17:52:56 +0000

    Main.py + TTS.py
    Main:
    added origin path and the cors middleware functions
    
    TTS.py:
    Re added basic gpt code for the text to speech

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List 
+
+app = FastAPI()
+
+origins = {
+    "http://localhost:3000"
+} 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/openai")
