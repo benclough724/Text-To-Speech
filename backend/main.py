50f42e810743fdc870e769c415a74afaa39241f1
benclough724 <ben_clough123@outlook.com>
2024-12-16 00:07:37 +0000

    Update main.py
    Updated main.py with the gpt logic for making a post request which returns the audio file

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List 
+from pathlib import Path
+from openai import OpenAI
 
 app = FastAPI()
+client = openAI()
 
+#Input model
+class TTSInput(BaseModel):
+    text: str
+
+# React website origin
 origins = {
     "http://localhost:3000"
 } 
 
+# add the cors middleware  
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,4 +27,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/openai")
+@app.post("/audio/")
+async def generate_audio():
+    try:
+        
+        speech_file_path = Path(__file__).parent / "speech.mp3"
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input="Today is a wonderful day to build something people love!"
+)
+response.stream_to_file(speech_file_path)
